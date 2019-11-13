3f9a7574cc4ac140f645828ed025df3ada007d71
Tiffanyti <anotherpastels@gmail.com>
2019-11-13 17:39:28 +0900

    mencoba alert confirm prompt

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 var x = 10;
 console.log('hello world!');
 console.log('isi dari variable x adalah' + x);
+
+alert('first alert');
+alert('coba sekali lagi');
+
+var nama = prompt('namanya siapa?');
+alert(nama);
+
+var tes = confirm('yakin?');
+
+if ( tes == true ) {
+	alert('user oke');
+}	
+else {
+	alert('user cancel');
+}
