a242fbc2956487e67fefef8b4124f0e3a404a28c
Tiffanyti <anotherpastels@gmail.com>
2019-11-14 20:22:16 +0900

    mencoba control flow js yang pengulangan

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
-var x = 10;
-console.log('hello world!');
-console.log('isi dari variable x adalah' + x);
+// var x = 10;
+// console.log('hello world!');
+// console.log('isi dari variable x adalah' + x);
 
-alert('first alert');
-alert('coba sekali lagi');
+// alert('first alert');
+// alert('coba sekali lagi');
 
-var nama = prompt('namanya siapa?');
-alert(nama + ' ya');
+// var nama = prompt('namanya siapa?');
+// alert(nama + ' ya');
 
-var tes = confirm('yakin?');
+// var tes = confirm('yakin?');
 
-if ( tes == true ) {
-	alert('user oke');
-}	
-else {
-	alert('user cancel');
+// if ( tes == true ) {
+// 	alert('user oke');
+// }	
+// else {
+// 	alert('user cancel');
+// }
+
+// alert('tunggu');
+// var lagi = true;
+
+// while (lagi == true) {
+// 	var nama = prompt('namanya');
+// 	alert('halo ' + nama);
+
+// 	lagi = confirm('coba lagi?');
+// }
+
+// alert('terima kasih');
+
+
+
+// for (var i = 0; i < 5; i++) {
+// 	alert('coba pake "for" sampe 5x');
+// 
+
+
+var nilaiAwal = 1;
+while(nilaiAwal <=5) {
+	console.log ('hello world' + nilaiAwal);
+	nilaiAwal++;
 }
-
-alert('tunggu');
-var lagi = true;
-
-while (lagi == true) {
-	var nama = prompt('namanya');
-	alert('halo ' + nama);
-
-	lagi = confirm('coba lagi?');
-}
-
-alert('terima kasih');
