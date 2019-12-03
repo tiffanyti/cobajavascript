d4fdd19588fe9563634a7fb4ce33ea52ca337de2
Tiffanyti <anotherpastels@gmail.com>
2019-12-03 15:48:41 +0900

    mencoba test while dan menggabungkan for dan while

--- script.js
+++ script.js
@@ -36,8 +36,21 @@
 // 
 
 
-var nilaiAwal = 1;
-while(nilaiAwal <=5) {
-	console.log ('hello world' + nilaiAwal);
-	nilaiAwal++;
+// var nilaiAwal = 1;
+// while(nilaiAwal <=5) {
+// 	console.log ('hello world ' + nilaiAwal);
+// 	nilaiAwal++;
+// }
+
+var jmlAngkot = 10;
+var angkotjalan = 6;
+var noangkot =1;
+
+while(noangkot<=angkotjalan) {
+	console.log ( 'angkot ' + noangkot + ' jalan');
+noangkot++;
+}
+
+for(noangkot = angkotjalan+1; noangkot <= jmlAngkot; noangkot++) {
+	console.log ( 'angkot ' + noangkot + ' ga jalan');
 }
