3c2038bf07407d884726b3155334ea1e66edea17
Tiffanyti <anotherpastels@gmail.com>
2019-12-13 13:37:51 +0900

    belajar teori array dan object dasar

--- function.js
+++ function.js
@@ -6,13 +6,38 @@
 
 // console.log(hitungan(2,3));
 
-function arraynya() {
-	var hasil = 0;
-	for ( var i=0; i<arguments.length;i++) {
-		hasil += arguments[i];
-	}
-	return hasil;
+
+
+// function arraynya() {
+// 	var hasil = 0;
+// 	for ( var i=0; i<arguments.length;i++) {
+// 		hasil += arguments[i];
+// 	}
+// 	return hasil;
+// }
+
+// var coba = arraynya(3,3,3,3);
+// console.log(coba);
+
+
+// function faktorial(n){
+// 	if (n===0) return 1;
+// 	return n*faktorial(n-1);
+// }
+
+// console.log(faktorial(5));
+
+
+
+var binatang=["kucing","anjing","bebek"];
+console.log(binatang[1]);
+console.log(typeof(binatang));
+console.log(binatang.length);
+
+function punpun(){
+alert("hello");
 }
 
-var coba = arraynya(3,3,3,3);
-console.log(coba);
+var campur=[3,4,'err',punpun, [2,5,6]];
+
+console.log(campur[4][1]);
---
+++ object.js
@@ -0,0 +1,14 @@
+var mhs = {
+	nama : "ティファニー",
+	umur : 26,
+	sekolah : "lulus",
+	nomor : [3,4,5,6],
+	alamat : {
+		jalan : "j.abc",
+		kota : "jakarta",
+		provinsi : "jawa",
+
+	// sapa : function() {
+	// 	return "halo, saya " + this.nama;
+	}
+};
