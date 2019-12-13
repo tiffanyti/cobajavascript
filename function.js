f8e12e862ec5cc8f4c28e64c3cb1008ff6cf7c1e
Tiffanyti <anotherpastels@gmail.com>
2019-12-13 20:09:24 +0900

    belajar array dan sedikit method kek length, join, push pop shift unshift, dan slice splice

--- function.js
+++ function.js
@@ -29,15 +29,58 @@
 
 
 
-var binatang=["kucing","anjing","bebek"];
-console.log(binatang[1]);
-console.log(typeof(binatang));
-console.log(binatang.length);
+// var binatang=["kucing","anjing","bebek"];
+// console.log(binatang[1]);
+// console.log(typeof(binatang));
+// console.log(binatang.length);
 
-function punpun(){
-alert("hello");
-}
+// function punpun(){
+// alert("hello");
+// }
 
-var campur=[3,4,'err',punpun, [2,5,6]];
+// var campur=[3,4,'err',punpun, [2,5,6]];
 
-console.log(campur[4][1]);
+// console.log(campur[4][1]);
+
+
+//nambah array
+var arr = [];
+arr[0]=2;
+arr[1]=33;
+arr[2]=4;
+arr[3]=5;
+
+console.log(arr[1]);
+
+
+// // hapus array
+// var arra=[3,4,5,6,7];
+// arra[3]=undefined;
+
+// console.log(arra);
+
+
+// // menampilkan isi array
+// for(var i=0; i<5;i++) {
+// 	console.log("nomor " + (i+1) + " adalah " + arra[i]);
+// }
+
+
+// // push and pop
+// arr.push(9);
+// arr.pop();
+
+// // unshift and shift
+// arr.unshift(9);
+// arr.shift();
+
+
+// // splice and slice
+// arr.splice(0,0,8,3);
+// arr.splice(0,1,7);
+
+// var arr22= arr.slice(2,4);
+
+//  // join
+// console.log(arr.join("-"));
+// console.log(arr22.join(" "));
