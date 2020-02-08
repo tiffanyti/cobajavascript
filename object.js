7e90290f53fec5773726abb9adeef39f466faa92
Tiffanyti <anotherpastels@gmail.com>
2020-02-08 12:04:26 +0900

    akhirnya beres

--- object.js
+++ object.js
@@ -33,35 +33,40 @@ function daftarmhsnya(nama,umur,sekolah,nomor,alamat) {
 	siswa.umur = umur;
 	siswa.sekolah = sekolah;
 	siswa.nomor = nomor;
+ 	siswa.alamat = {}; // define parent first
 
-	siswa.alamat.jalan = 0;
-	siswa.alamat.kota = 1;
-	siswa.alamat.provinsi = 2;
+	siswa.alamat.jalan = alamat[0];
+	siswa.alamat.kota = alamat[1];
+	siswa.alamat.provinsi = alamat[2];
 	
 
 	return siswa;
 }
 
 
-var mhs3 = daftarmhsnya( "あか" , "25" , "lulus" ,[3,4,5,6],["jl.agg" , "jakarta" , "palu"] );
+var mhs3 = daftarmhsnya("あか", "25","lulus",[45,100,200,100],["jl.agg","jakarta" ,"palu"]);
 
 
-// // concstructor (nama harus huruf besar)
-// function Mahasiswa(nama,umur,sekolah,nomor,alamat) {
-// 	// var dan return udah otomatis kalo pake constructor
-// 	this.nama = nama;
-// 	this.umur = umur;
-// 	this.sekolah = sekolah;
-// 	this.nomor = nomor;
-	
-// 	this.alamat.jalan = 0;
-// 	this.alamat.kota = 1;
-// 	this.alamat.provinsi = 2;
+// // constructor (nama harus huruf besar)
+function Mahasiswa(nama,umur,sekolah,nomor,alamat) {
 
-// }
+	// var dan return udah otomatis kalo pake constructor
+	this.nama = nama;
+	this.umur = umur;
+	this.sekolah = sekolah;
+	this.nomor = nomor;
+	this.alamat = {};
+	this.alamat.jalan = alamat[0];
+	this.alamat.kota = alamat[1];
+	this.alamat.provinsi = alamat[2];
 
-// var mhs4 = new Mahasiswa("エリ" , "22" , "lulus" ,[3,4,5,6],["jl.agfg" , "jakarta" , "kupang"] );
+}
 
+var mhs4 = new Mahasiswa("エリ" , "22" , "lulus" ,[3,4,5,6],["jl.agfg" , "jakarta" , "kupang"] );
+
+var new_car = new car(); // call new class constructor
+
+new_car.add_car('toyota','red','MT');
 
 
 
